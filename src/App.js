3bddf78c8f073f4666b2857e9b723a94369c6973
Leontine Claassen <leon.claassen00@gmail.com>
2021-10-08 11:35:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 import './Sass/main.css'
+import { FontProvider } from './components/FontContext'
+
 
 const App = () => {
+  const [fontSize, setFontSize] = useState(20);
+  const value = {fontSize, setFontSize};
+
   return (
     <div> 
+   <FontProvider>
+    <Navbar />
+    
+   
     <Header />
-      
+   </FontProvider>
     </div>
-  )
+  );
 }
 
 export default App
