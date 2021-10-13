6e57cc04c8fe708fb178e74232d52c670f721a72
CarmenMihaela <71848618+CarmenMihaela@users.noreply.github.com>
2021-10-13 17:23:10 +0200

    some styiling added to book details modal

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
-import { useState, useEffect } from "react"
+import { FaWindowClose } from "react-icons/fa"
 
 
 const BookDetails = ({ open, children, onClose, title, author, omslagafbeeldingen, publisher, language, series, description}) => {
@@ -13,17 +13,24 @@ const BookDetails = ({ open, children, onClose, title, author, omslagafbeeldinge
         <>
         <div className="overlay"/>
         <div className="modal">
-        <button 
+        <button class="close-window"
         onClick={onClose} 
-        >Close Modal </button>
+        ><FaWindowClose class="icon"/>Sluiten </button>
             {children}
+            <h3>{title} bij {author.slice(author.indexOf(',')+1, author.length)} {author.slice(0, author.indexOf(','))}</h3>
+            <div className="modal_display">
+            <div className="modal_image">
             <img src={omslagafbeeldingen} alt={"cover picture of " + title} />
+            </div>
+            <div className="modal_details">
             <p>Titel: {title}</p>
             <p>Auteur: {author}</p>
             <p>Uitgever: {publisher}</p>
-            <p>Language: {language}</p>
-            <p>Series: {series}</p>
-            <p>Description: {description}</p>
+            <p>Taal: {language}</p>
+            <p>Reeks: {series}</p>
+            <p>Beschrijving: {description}</p>
+            </div>
+        </div>
         </div>
         </>,
         document.getElementById('portal')
