c41f6dbdcb24a58fd5e74703e50b7873ae0e4fd2
Leontine Claassen <leon.claassen00@gmail.com>
2021-10-15 09:56:28 +0200

    changes to slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,56 +1,98 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
 import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 
-const Slider = ({ genre }) => {
-  const [sliderData, setSliderData] = useState();
-  // const [current, setCurrent] = useState(0);
+const Slider = ({ data }) => {
+  const [current, setCurrent] = useState(0);
 
-  useEffect(() => {
-    fetch(
-      `https://techgrounds-oba.herokuapp.com/api/oba/techgrounds/boeken/`
-    )
-      .then((response) => response.json())
-      .then((newData) => {
-        setSliderData(newData.boeken);
-      });
-  }, [genre]);
+  if (data.length > 10) data.length = 10;
 
-  if (!sliderData) return null;
+  const length = data.length;
+  console.log(length);
+  // console.log(data[0].title)
 
-  console.log(sliderData.length);
-  if (sliderData.length > 10) sliderData.length = 10;
-  const lengthSlider = sliderData.length;
-  console.log(lengthSlider);
-
-  /*const currentIndexes = (current) => {
-    if (current > lengthSlider - 1) {
-      return console.error('current index cannot be more than array length');
-    }
-    return [
-      current,
-      (current + 1) % lengthSlider,
-      (current + 2) % lengthSlider,
-      (current + 3) % lengthSlider,
-      (current + 4) % lengthSlider,
-    ];
-  }; */
+  const currentIndex = (current) => {
+  if (current > (length - 1)) {
+    return null;
+  }
   
-  // const nextSlide = () => {  //if length-1 this is last slide so returns to 0= first slide continuous loop
-    // setCurrent(current === lengthSlider - 1 ? 0 : current + 1);   //else current + next in array
-  // };
+  return [
+    current,
+    (current + 1) % length,
+    (current + 2) % length,
+    (current + 3) % length,
 
-  // const prevSlide = () => {    //if current is first slide count backwards in array from first 
-    // setCurrent(current === 0 ? lengthSlider - 1 : current - 1); //else count backwards from slide your on now
-  // };
+  ];
+}; 
 
-
+  const nextSlide = () => {
+    //if length-1 this is last slide so returns to 0= first slide continuous loop
+    setCurrent(current === length - 1 ? 0 : current + 1); //else current + next in array
+  };
   
 
+  const prevSlide = () => {
+    //if current is first slide count backwards in array from first
+    setCurrent(current === 0 ? length - 1 : current - 1); //else count backwards from slide your on now
+  };
+
+  console.log(current);
+
+  if (!Array.isArray(data) || data.length <= 0) {
+    return null; //in case no array or no slides
+  }
 
   return (
     <>
-   <section className="slider">
+      <section className="slider">
+        <button className="arrowBtnLeft">
+          <ArrowLeftIcon style={{ fontSize: "200px" }} onClick={prevSlide} />
+        </button>
+        <button className="arrowBtnRight">
+          <ArrowRightIcon style={{ fontSize: "200px" }} onClick={nextSlide} />
+        </button>
+
+
+        <div className="sliderContainer">
+          {data.map((slide, index) => {
+            return (
+              <div
+                className={index === current ? "slide active" : "slide"}
+                key={index}
+              >
+                {index === current && (
+                  <img
+                    src={slide.omslagafbeeldingen[1]}
+                    alt=""
+                    className="sliderImage"
+                  />
+                )}
+                {slide.title}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+/* import React, {useState} from 'react';
+import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+
+const Slider = ({data}) => {
+ const [current, setCurrent] = useState();
+
+  
+  if (data.length > 10) data.length = 10;
+
+ const length = data.length
+ console.log(length)
+console.log(data[0].title)
+
+  return (
+    <>
+     <section className="slider">
         <button className="arrowBtnLeft">
           <ArrowLeftIcon style={{ fontSize: "200px" }}  />
         </button>
@@ -60,7 +102,7 @@ const Slider = ({ genre }) => {
 
        
 
-        {sliderData.audio.map(({ title, omslagafbeeldingen }) => (
+        {data.map(({ title, omslagafbeeldingen }) => (
           <div className="sliderContainer">
             <img src={omslagafbeeldingen[1]} alt="" className="sliderImage" />
             {title}
@@ -68,7 +110,6 @@ const Slider = ({ genre }) => {
         ))} 
       </section>
     </>
-  );
-};
-
+  )
+}*/
 export default Slider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider";
 
 const Home = () => {
-  
+  const [sliderData, setSliderData] = useState();
+
+  useEffect(() => {
+    fetch(`https://techgrounds-oba.herokuapp.com/api/oba/techgrounds/boeken/`)
+      .then((response) => response.json())
+      .then((newData) => {
+        console.log(newData);
+        setSliderData(newData);
+      });
+  }, []);
+
+  if (!sliderData) return null;
 
   
 
-  
-
-  
-  
-
+console.log(sliderData.largetype.boeken)
 
   return (
     <div>
-      <Slider />
-      
-      {/* <Slider genre="largetype" /> */}
-      
-      {/* <Slider genre="e" /> */}
+      <Slider  data={sliderData.largetype.boeken} />
+      <Slider data={sliderData.audio.boeken} />
+      <Slider data={sliderData.book.boeken} />
     </div>
   );
 };
