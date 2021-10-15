6e756eda4ab643793d7ca40bc93102fd4bd7940c
MaherMarawi <mahermarawi92@gmail.com>
2021-10-15 14:22:38 +0200

    router install

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
-// import Header from './components/Header'
-import './Sass/main.css'
-import Home from './pages/Home'
-=======
 import React, {useState} from 'react'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import SearchResultsBooks from './pages/SearchResultsBooks'
 import './Sass/main.css'
 import { FontProvider } from './components/FontContext'
->>>>>>> 474a653fd8c165ce2b56f9bf2be62ee792360cfa
 
 
 const App = () => {
@@ -19,17 +12,13 @@ const App = () => {
 
   return (
     <div> 
-<<<<<<< HEAD
     
-      <Home />
-=======
    <FontProvider>
     <Navbar />
     <Header />
    </FontProvider>
     <SearchResultsBooks />
       
->>>>>>> 474a653fd8c165ce2b56f9bf2be62ee792360cfa
     </div>
   );
 }
