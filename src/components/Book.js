a97885af9111dfbc5772975d5d91e7e78b9ad80c
LeontineC <79845918+LeontineC@users.noreply.github.com>
2021-10-27 11:16:44 +0200

    responsiveness activiteiten

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,29 +1,37 @@
-import { useState } from "react"
-import BookDetails from "./BookDetails"
+import { useState } from "react";
+import BookDetails from "./BookDetails";
 
-const Book = ({ title, omslagafbeeldingen, author, publisher, language, series, description }) => {
+const Book = ({
+  title,
+  omslagafbeeldingen,
+  author,
+  publisher,
+  language,
+  series,
+  description,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <article>
+      <button className="sliderImage" onClick={() => setIsOpen(true)}>
+        {" "}
+        <img className="sliderImages" src={omslagafbeeldingen} alt={"cover picture of " + title} />
+        <h5 className="sliderTitle" >{title}</h5>
+      </button>
 
-    const [isOpen, setIsOpen] = useState(false)
-    return (
+      <BookDetails
+        omslagafbeeldingen={omslagafbeeldingen}
+        title={title}
+        author={author}
+        publisher={publisher}
+        language={language}
+        series={series}
+        description={description}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      ></BookDetails>
+    </article>
+  );
+};
 
-        <article >
-            <button className="sliderImage" onClick={() => setIsOpen(true)}> <img src={omslagafbeeldingen} alt={"cover picture of " + title} /> 
-            <h5>{title}</h5>
-            </button>
-            
-            <BookDetails 
-            omslagafbeeldingen={omslagafbeeldingen}
-            title={title} 
-            author={author}
-            publisher={publisher}
-            language={language}
-            series={series}
-            description={description}
-            open={isOpen} onClose={() => setIsOpen(false)}>
-            </BookDetails>
-        </article>
-    )
-}
-
-export default Book
-
+export default Book;
